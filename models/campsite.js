ce15c3ab1fb6bf17c84e8ed21ae0946e732b337f
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-05-19 20:15:18 -0500

    added model folder

---
+++ models/campsite.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//schema 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+
+//model for the schema 
+const Campsite = mongoose.model('Campsite', campsiteSchema);
+
+module.exports = Campsite;
