846f1c6405248e41da5c179b519c1b5e9e461e4c
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-05-22 10:41:55 -0500

    auth to router

--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,43 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
+const passport = require('passport');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup', (req, res, next)=> {
-  User.findOne({username: req.body.username})
-  .then(user => {
-    if (user) {
-      const err = new Error(`User ${req.body.username} already exists!`);
-      err.status = 403;
-      return next(err);
-    } else {
-      User.create({
-        username: req.body.username,
-        password: req.body.password})
-        .then(user => {
+//updating router to use passportjs w/ authentication
+router.post('/signup', (req, res)=> {
+  User.register(
+    new User({username: req.body.username}),
+    req.body.password,
+    err => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({err: err});
+      } else {
+        passport.authenticate('local')(req, res, ()=> {
           res.statusCode = 200;
           res.setHeader('Content-Type', 'application/json');
-          res.json({status: 'Registration Successful!', user: user});
-        })
-        .catch(err => next(err));
+          res.json({success: true, status: 'Registration Successful!'});
+        });
+      }
     }
-  })
-  .catch(err => next(err));
+  );
 });
 
+router.post('/login', passport.authenticate('local'), (req, res)=> {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, status: 'You are successfully logged in!'});
+});
+
+/*
+old login
 router.post('/login', (req, res, next)=> {
   if(!req.session.user) {
     const authHeader = req.headers.authorization;
@@ -68,6 +77,9 @@ router.post('/login', (req, res, next)=> {
     res.end('You are already authenticated!');
   }
 });
+*/
+
+
 
 router.get('/logout', (req, res, next)=> {
   if(req.session) {
